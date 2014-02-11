01c058f68be7fe0e9457eaae1b0ca7056397fe13
fool2fish <fool2fish@gmail.com>
2014-02-11 14:34:00 +0800

    Replace indent of 4 spaces with 2 spaces.

--- static/labor.js
+++ static/labor.js
@@ -1,288 +1,288 @@
 (function() {
-    if ( typeof console === 'undefined') {
-        console = {
-            log: function(msg) {
-                var el = document.createElement('div')
-                el.innerHTML = msg
-                document.body.appendChild(el)
+  if ( typeof console === 'undefined') {
+    console = {
+      log: function(msg) {
+        var el = document.createElement('div')
+        el.innerHTML = msg
+        document.body.appendChild(el)
 
-                setTimeout(function() {
-                    document.body.removeChild(el)
-                }, 300000)
-            }
-        }
+        setTimeout(function() {
+          document.body.removeChild(el)
+        }, 300000)
+      }
+    }
+  }
+
+  function Labor() {
+    this.orders = {}
+    this.reports = []
+    this.timer = undefined
+    this.init()
+  }
+
+
+  Labor.prototype.init = function() {
+    var that = this
+    var socket = this.socket = io.connect('/labor')
+
+    socket.on('connect', function() {
+      console.log('connected')
+      socket.emit('init', {
+        ua: navigator.userAgent
+      })
+    })
+
+    socket.on('ping', function() {
+      clearInterval(that.timer)
+    })
+
+    socket.on('add', function(data) {
+      that.add(data)
+    })
+
+    socket.on('remove', function(orderId) {
+      that.remove(orderId)
+    })
+
+    socket.on('disconnect', function() {
+      console.log('disconnected')
+      for (var i in that.orders) {
+        that.remove(i)
+      }
+      that.timer = setInterval(function() {
+        that.socket.emit('ping')
+      }, 950)
+    })
+
+    setInterval(function() {
+      if (that.reports.length) {
+        var data = that.reports
+        that.reports = []
+        that.socket.emit('report', data)
+      }
+    }, 1000)
+  }
+
+  Labor.prototype.add = function(data) {
+    var orderId = data.orderId
+    var path = data.href.replace(/https?\:\/\/[^/]+?\//, '/')
+    var src = '/runner/' + data.orderId + path
+    var element
+
+    if (data.uaGroup === 'mobile') {
+      element = document.createElement('iframe')
+      element.src = src
+      document.body.appendChild(element)
+    } else {
+      element = window.open(src, 'totoro_' + (new Date()).getTime(), 'top=100,left=200,width=800,height=600')
     }
 
-    function Labor() {
-        this.orders = {}
-        this.reports = []
-        this.timer = undefined
-        this.init()
+    this.orders[orderId] = element
+    this.orders[orderId].verbose = data.verbose
+
+    console.log('add order: ' + src)
+  }
+
+  Labor.prototype.remove = function(orderId) {
+    var element = this.orders[orderId]
+
+    if (element) {
+      delete this.orders[orderId]
+
+      if (element.nodeName) {
+        document.body.removeChild(element)
+      } else {
+        element.close()
+      }
+
+      console.log('remove order: ' + orderId)
     }
+  }
 
 
-    Labor.prototype.init = function() {
-        var that = this
-        var socket = this.socket = io.connect('/labor')
+  var labor = new Labor()
 
-        socket.on('connect', function() {
-            console.log('connected')
-            socket.emit('init', {
-                ua: navigator.userAgent
-            })
-        })
 
-        socket.on('ping', function() {
-            clearInterval(that.timer)
-        })
+  window.totoro = {
+    report: function(data) {
+      data = clone(data)
 
-        socket.on('add', function(data) {
-            that.add(data)
-        })
-
-        socket.on('remove', function(orderId) {
-            that.remove(orderId)
-        })
-
-        socket.on('disconnect', function() {
-            console.log('disconnected')
-            for (var i in that.orders) {
-                that.remove(i)
-            }
-            that.timer = setInterval(function() {
-                that.socket.emit('ping')
-            }, 950)
-        })
-
-        setInterval(function() {
-            if (that.reports.length) {
-                var data = that.reports
-                that.reports = []
-                that.socket.emit('report', data)
-            }
-        }, 1000)
-    }
-
-    Labor.prototype.add = function(data) {
+      if (data.action === 'end') {
         var orderId = data.orderId
-        var path = data.href.replace(/https?\:\/\/[^/]+?\//, '/')
-        var src = '/runner/' + data.orderId + path
-        var element
+        console.log('finish order: ' + orderId)
 
-        if (data.uaGroup === 'mobile') {
-            element = document.createElement('iframe')
-            element.src = src
-            document.body.appendChild(element)
-        } else {
-            element = window.open(src, 'totoro_' + (new Date()).getTime(), 'top=100,left=200,width=800,height=600')
+
+        if (!(data.info && data.info.error)) {
+          var element = labor.orders[orderId]
+          var verbose = element.verbose
+
+          var jscoverage = element._$jscoverage ||
+            (element.contentWindow && element.contentWindow._$jscoverage)
+
+          if (jscoverage) {
+            var cov = map(jscoverage, verbose)
+            ;delete cov.files
+            data.info.coverage = cov
+          }
         }
 
-        this.orders[orderId] = element
-        this.orders[orderId].verbose = data.verbose
+        labor.remove(orderId)
+      }
 
-        console.log('add order: ' + src)
+      labor.reports.push(data)
+    }
+  }
+
+  /*
+   * deep clone data from crossed window, not orthodox
+   * see #33, #45, docs/type-checking
+   */
+  function clone(obj) {
+    if (obj && obj.toString) {
+      var isPlainObj
+      var fnReg = /^function[^\(]*\([^\)]*\)/
+      var fnMatched
+      var objstr = obj.toString()
+
+      // plain object or array
+      /*
+       * NOTE
+       * must decide if obj is an array, because:
+       * [{...}].toString() -> [object Object]
+       */
+      if (obj.length >= 0 && obj.splice || objstr === '[object Object]') {
+        var rt = obj.length >= 0 && obj.splice ? [] : {}
+        for (var i in obj) {
+          rt[i] = clone(obj[i])
+        }
+        return rt
+      // function
+      } else if (obj.prototype && (fnMatched = objstr.match(fnReg))) {
+        return fnMatched[0] + ' {...}'
+      // DOM
+      } else if (obj.nodeName && obj.nodeType) {
+        return '<' +
+          obj.nodeName.toLowerCase() +
+          (obj.id ? ' id="' + obj.id + '"': '') +
+          (obj.className ? ' class="' + obj.className + '"' : '') +
+          ' />'
+      } else {
+        return objstr
+      }
+    } else {
+      return obj
+    }
+  }
+
+
+  // the following code base on
+  // https://github.com/visionmedia/mocha/blob/master/lib/reporters/json-cov.js
+
+  /**
+   * Map jscoverage data to a JSON structure
+   * suitable for reporting.
+   *
+   * @param {Object} cov
+   * @return {Object}
+   * @api private
+   */
+  function map(cov, verbose) {
+    var ret = {
+      sloc: 0,
+      hits: 0,
+      misses: 0,
+      coverage: 0,
+      files: []
     }
 
-    Labor.prototype.remove = function(orderId) {
-        var element = this.orders[orderId]
+    var missesDetail = {}
 
-        if (element) {
-            delete this.orders[orderId]
+    for (var filename in cov) {
+      var data = coverage(filename, cov[filename])
+      ret.files.push(data)
+      ret.hits += data.hits
 
-            if (element.nodeName) {
-                document.body.removeChild(element)
-            } else {
-                element.close()
-            }
+      if (data.misses.length) {
+        missesDetail[filename] = data.misses
+      }
 
-            console.log('remove order: ' + orderId)
-        }
+      ret.misses += data.misses.length
+      ret.sloc += data.sloc
     }
 
+    ret.files.sort(function(a, b) {
+      return a.filename.localeCompare(b.filename)
+    });
 
-    var labor = new Labor()
+    if (ret.sloc > 0) {
+      ret.coverage = (ret.hits / ret.sloc) * 100
 
-
-    window.totoro = {
-        report: function(data) {
-            data = clone(data)
-
-            if (data.action === 'end') {
-                var orderId = data.orderId
-                console.log('finish order: ' + orderId)
-
-
-                if (!(data.info && data.info.error)) {
-                    var element = labor.orders[orderId]
-                    var verbose = element.verbose
-
-                    var jscoverage = element._$jscoverage ||
-                        (element.contentWindow && element.contentWindow._$jscoverage)
-
-                    if (jscoverage) {
-                        var cov = map(jscoverage, verbose)
-                        ;delete cov.files
-                        data.info.coverage = cov
-                    }
-                }
-
-                labor.remove(orderId)
-            }
-
-            labor.reports.push(data)
-        }
+      if (verbose) {
+        ret['missesDetail'] = missesDetail
+      }
     }
 
-    /*
-     * deep clone data from crossed window, not orthodox
-     * see #33, #45, docs/type-checking
-     */
-    function clone(obj) {
-        if (obj && obj.toString) {
-            var isPlainObj
-            var fnReg = /^function[^\(]*\([^\)]*\)/
-            var fnMatched
-            var objstr = obj.toString()
+    return ret
+  }
 
-            // plain object or array
-            /*
-             * NOTE
-             * must decide if obj is an array, because:
-             * [{...}].toString() -> [object Object]
-             */
-            if (obj.length >= 0 && obj.splice || objstr === '[object Object]') {
-                var rt = obj.length >= 0 && obj.splice ? [] : {}
-                for (var i in obj) {
-                    rt[i] = clone(obj[i])
-                }
-                return rt
-            // function
-            } else if (obj.prototype && (fnMatched = objstr.match(fnReg))) {
-                return fnMatched[0] + ' {...}'
-            // DOM
-            } else if (obj.nodeName && obj.nodeType) {
-                return '<' +
-                       obj.nodeName.toLowerCase() +
-                       (obj.id ? ' id="' + obj.id + '"': '') +
-                       (obj.className ? ' class="' + obj.className + '"' : '') +
-                       ' />'
-            } else {
-                return objstr
-            }
-        } else {
-            return obj
-        }
+  /**
+   * Map jscoverage data for a single source file
+   * to a JSON structure suitable for reporting.
+   *
+   * @param {String} filename name of the source file
+   * @param {Object} data jscoverage coverage data
+   * @return {Object}
+   * @api private
+   */
+  function coverage(filename, data) {
+    var ret = {
+      filename: filename,
+      coverage: 0,
+      hits: 0,
+      misses: [],
+      sloc: 0,
+      source: {}
+    };
+
+    for (var i = 0; i < data.source.length; i++) {
+      var num = i
+      var line = data.source[i]
+
+      num++
+
+      if (data[num] === 0) {
+        ret.misses.push(num)
+        ret.sloc++
+      } else if (data[num] !== undefined) {
+        ret.hits++
+        ret.sloc++
+      }
+
+      ret.source[num] = {
+        source: line,
+        coverage: data[num] === undefined ? '' : data[num]
+      }
     }
 
+    ret.coverage = ret.hits / ret.sloc * 100
 
-    // the following code base on
-    // https://github.com/visionmedia/mocha/blob/master/lib/reporters/json-cov.js
+    return ret
+  }
 
-    /**
-     * Map jscoverage data to a JSON structure
-     * suitable for reporting.
-     *
-     * @param {Object} cov
-     * @return {Object}
-     * @api private
-     */
-    function map(cov, verbose) {
-        var ret = {
-            sloc: 0,
-            hits: 0,
-            misses: 0,
-            coverage: 0,
-            files: []
-        }
-
-        var missesDetail = {}
-
-        for (var filename in cov) {
-            var data = coverage(filename, cov[filename])
-            ret.files.push(data)
-            ret.hits += data.hits
-
-            if (data.misses.length) {
-                missesDetail[filename] = data.misses
-            }
-
-            ret.misses += data.misses.length
-            ret.sloc += data.sloc
-        }
-
-        ret.files.sort(function(a, b) {
-            return a.filename.localeCompare(b.filename)
-        });
-
-        if (ret.sloc > 0) {
-            ret.coverage = (ret.hits / ret.sloc) * 100
-
-            if (verbose) {
-                ret['missesDetail'] = missesDetail
-            }
-        }
-
-        return ret
+  /**
+   * Return a plain-object representation of `test`
+   * free of cyclic properties etc.
+   *
+   * @param {Object} test
+   * @return {Object}
+   * @api private
+   */
+  function clean(test) {
+    return {
+      title: test.title,
+      fullTitle: test.fullTitle(),
+      duration: test.duration
     }
-
-    /**
-     * Map jscoverage data for a single source file
-     * to a JSON structure suitable for reporting.
-     *
-     * @param {String} filename name of the source file
-     * @param {Object} data jscoverage coverage data
-     * @return {Object}
-     * @api private
-     */
-    function coverage(filename, data) {
-        var ret = {
-            filename: filename,
-            coverage: 0,
-            hits: 0,
-            misses: [],
-            sloc: 0,
-            source: {}
-        };
-
-        for (var i = 0; i < data.source.length; i++) {
-            var num = i
-            var line = data.source[i]
-
-            num++
-
-            if (data[num] === 0) {
-                ret.misses.push(num)
-                ret.sloc++
-            } else if (data[num] !== undefined) {
-                ret.hits++
-                ret.sloc++
-            }
-
-            ret.source[num] = {
-                source: line,
-                coverage: data[num] === undefined ? '' : data[num]
-            }
-        }
-
-        ret.coverage = ret.hits / ret.sloc * 100
-
-        return ret
-    }
-
-    /**
-     * Return a plain-object representation of `test`
-     * free of cyclic properties etc.
-     *
-     * @param {Object} test
-     * @return {Object}
-     * @api private
-     */
-    function clean(test) {
-        return {
-            title: test.title,
-            fullTitle: test.fullTitle(),
-            duration: test.duration
-        }
-    }
+  }
 
 })()
--- static/totoro.js
+++ static/totoro.js
@@ -1,47 +1,47 @@
 
 (function() {
-    var id = location.href.match(/runner\/([^/]+)\//)[1]
+  var id = location.href.match(/runner\/([^/]+)\//)[1]
 
-    window.totoro = (window.opener || window.top).totoro
+  window.totoro = (window.opener || window.top).totoro
 
-    totoro.end = function() {
-        totoro.report({
-            action: 'end',
-            orderId: id,
-            info: {}
-        })
-    }
-
-
-    if ( typeof console === 'undefined') {
-        console = {}
-    }
-
-    console.log = function() {
-        totoro.report({
-            action: 'log',
-            orderId: id,
-            info: [].slice.call(arguments, 0)
-        })
-    }
+  totoro.end = function() {
+    totoro.report({
+      action: 'end',
+      orderId: id,
+      info: {}
+    })
+  }
 
 
-    window.alert = function(){}
-    window.confirm = function(){return false}
-    window.prompt = function(){return null}
+  if ( typeof console === 'undefined') {
+      console = {}
+  }
 
-    window.onerror = function(message, url, line){
-        totoro.report({
-            orderId: id,
-            action: 'end',
-            info: {
-                error: {
-                    message: message,
-                    url: url,
-                    line: line
-                }
-            }
-        })
-        return true
-    }
+  console.log = function() {
+    totoro.report({
+      action: 'log',
+      orderId: id,
+      info: [].slice.call(arguments, 0)
+    })
+  }
+
+
+  window.alert = function(){}
+  window.confirm = function(){return false}
+  window.prompt = function(){return null}
+
+  window.onerror = function(message, url, line){
+    totoro.report({
+      orderId: id,
+      action: 'end',
+      info: {
+        error: {
+          message: message,
+          url: url,
+          line: line
+        }
+      }
+    })
+    return true
+  }
 })()
